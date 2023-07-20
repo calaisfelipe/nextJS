7662485d49492d2ae40509e84720c886ef4ea541
Felipe Calais <felipe_calais@hotmail.com>
2023-07-20 13:02:18 -0300

    msn clone project

--- msn-clone/components/ProfileDrawer.tsx
+++ msn-clone/components/ProfileDrawer.tsx
@@ -4,7 +4,8 @@ import { Conversation, User } from "@prisma/client";
 import useOtherUser from "@/app/hooks/useOtherUser";
 import { format } from "date-fns";
 import { Transition, Dialog } from "@headlessui/react";
-import { IoClose } from "react-icons/io5";
+import { IoClose, IoTrash } from "react-icons/io5";
+import Avatar from "./Avatar";
 
 type ProfileDrawerType = {
   data: Conversation & { users: User[] };
@@ -46,15 +47,15 @@ const ProfileDrawer = ({ data, isOpen, onClose }: ProfileDrawerType) => {
         </Transition.Child>
         <div className="fixed inset-0 overflow-hidden">
           <div className="absolute inset-0 overflow-hidden">
-            <div className="pointer-events-none fixed inset-y-0 flex max-w-full pl-10">
+            <div className="pointer-events-none fixed inset-y-0 flex max-w-full">
               <Transition.Child
                 as={Fragment}
                 enter="transform transition ease-in-out duration-500"
-                enterFrom="translate-x-full"
+                enterFrom="-translate-x-full"
                 enterTo="translate-x-0"
                 leave="transform transition ease-in-out duration-500"
                 //leaveFrom="translate-x-0"
-                leaveTo="translate-x-full"
+                leaveTo="-translate-x-full"
               >
                 <Dialog.Panel className="pointer-events-auto w-screen max-w-md">
                   <div className="flex h-full flex-col overflow-y-scroll py-06 bg-white shadow-xl">
@@ -73,6 +74,60 @@ const ProfileDrawer = ({ data, isOpen, onClose }: ProfileDrawerType) => {
                     
                         </div>
                       </div>
+                    </div>
+                    <div className="relative
+                    mt-6 flex-1 px-4 sm:px-6">
+                      <div className="flex flex-col items-center">
+                        <div className="mb-2 ">
+                          <Avatar user={otherUser}/>
+
+                        </div>
+                      <div>
+                        {title}
+                      </div>
+                      <div className="text-xs md:text-sm text-gray-500">
+                        {statusText}
+                      </div>
+                      <div className="flex gap-10 my-8"> 
+                          <div onClick={() => {}}
+                          className="flex flex-col cursor-pointer items-center hover:opacity-75">
+                            <div className="w-10 h-10 bg-neutral-100 flex rounded-full items-center justify-center">
+                                <IoTrash size={20} />
+                            </div>
+                            <div className="text-xs md:text-sm text-neutral-600 font-light">
+                              Delete
+                            </div>
+                          </div>
+                      </div>
+                      <div className="w-full pt-5 pb-5 sm:px-0 sm:pt-0 ">
+                          <dl className="space-y-8 px-4 sm:space-y-6 sm:px-6">
+                          {!data.isGroup && (
+                            <div>
+                              <dt className="text-sm font-medium text-gray-500 sm:w-40 sm:flex-shrink-0">
+                                Email
+                              </dt>
+                              <dd className="mt-1 text-sm text-gray-900 sm-col-span-2">
+                                {otherUser.email}
+                              </dd>
+                            </div>
+                          )}
+                          {!data.isGroup && (
+                            <>
+                            <hr />
+                            <div>
+                              <dt className="text-sm font-medium text-gray-500 sm:w-40 sm:flex-shrink-0">
+                                Joined
+                              </dt>
+                              <dd className="mt-1 text-sm text-gray-900 sm-col-span-2">
+                                <time dateTime={joinedDate}>{joinedDate}</time>
+                              </dd>
+                            </div>
+                            </>
+                          )}
+                          </dl>
+                      </div>
+                      </div>
+
                     </div>
                   </div>
                 </Dialog.Panel>
