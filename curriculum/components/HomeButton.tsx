be6ac6c4561cb8deec3889232c19dd857ba1bd67
Felipe Calais <felipe_calais@hotmail.com>
2023-09-21 16:15:38 -0300

    layout adjusts

--- curriculum/components/HomeButton.tsx
+++ curriculum/components/HomeButton.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {BsDownload} from 'react-icons/bs'
+import { BsDownload } from "react-icons/bs";
 
 type HomeButtonType = {
   text?: string;
@@ -24,7 +24,7 @@ const HomeButton = ({
     <>
       <button
         disabled={disabled}
-        className={`relative inline-block  bg-white font-medium text-[17px] mt-2 z-10 rounded-[8px]  duration-500 overflow-hidden w-[180px] 
+        className={`relative inline-block  bg-white font-medium text-[17px] mt-2 z-10 rounded-[8px]  duration-500 overflow-hidden w-[160px] md:w-[180px] 
     
     before:absolute  before:h-[250px] before:w-[400px] before:rounded-[20%] before:-z-10 before:content-[''] before:top-[100%] before:left-[100%] before:duration-700 
     
@@ -36,11 +36,21 @@ const HomeButton = ({
    
     border-2  
     
-    ${resume && "text-gray-400 before:bg-gray-600 border-gray-400 md:px-[24px] md:py-[16px] px-[12px] py-[10px] before:bg-opacity-90 "}
+    ${
+      resume &&
+      "text-gray-400 before:bg-gray-600 border-gray-400 md:px-[24px] md:py-[16px] px-[12px] py-[10px] before:bg-opacity-90 "
+    }
 
-    ${secondary && "text-black before:bg-black  border-white px-[18px] py-[12px] dark:border-blue-400 dark:text-blue-400 dark:before:bg-blue-400 dark:hover:text-white"}
+    ${
+      secondary &&
+      "text-black before:bg-black  border-white px-[18px] py-[12px] dark:border-blue-400 dark:text-blue-400 dark:before:bg-blue-400 dark:hover:text-white"
+    }
 
-    ${!secondary && !resume && "text-cyan-700 before:bg-cyan-700 border-cyan-700 md:px-[24px] md:py-[16px] px-[12px] py-[10px] dark:border-blue-400 dark:text-blue-400 dark:before:bg-blue-400 dark:hover:text-white"}
+    ${
+      !secondary &&
+      !resume &&
+      "text-cyan-700 before:bg-cyan-700 border-cyan-700 md:px-[24px] md:py-[16px] px-[12px] py-[10px] dark:border-blue-400 dark:text-blue-400 dark:before:bg-blue-400 dark:hover:text-white"
+    }
 
     disabled:bg-slate-500 disabled:bg-opacity-30 disabled:pointer-events-none
 
@@ -50,7 +60,8 @@ const HomeButton = ({
         onClick={action}
         type={type}
       >
-        {text}{resume && <BsDownload size={18} className='font-bold' /> }
+        {text}
+        {resume && <BsDownload size={18} className="font-bold" />}
         {children}
       </button>
     </>
