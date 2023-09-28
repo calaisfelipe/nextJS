369e7055bf8b91f7984c3343f33e6b68e0f60432
Felipe Calais <felipe_calais@hotmail.com>
2023-09-28 12:01:10 -0300

    deploy twitter-clone

--- twitter-clone/app/api/auth/[...nextauth]/options.ts
+++ twitter-clone/app/api/auth/[...nextauth]/options.ts
@@ -6,6 +6,7 @@ import { PrismaAdapter } from "@auth/prisma-adapter";
 import bcrypt from "bcrypt";
 
 const options: NextAuthOptions = {
+  //@ts-ignore
   adapter:PrismaAdapter(prismadb),
   providers: [
     CredentialsProvider({
